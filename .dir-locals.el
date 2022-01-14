904f948a5630eef498ec2064d82bf71d313d0f30
Michael Pontus <m.pontus@gmail.com>
2022-01-14 06:45:16 +0300

    Cleanup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nix-mode . ((eval .
-                    (progn
-                      (add-hook 'after-save-hook 'recompile nil 'local)
-                      (add-hook 'org-babel-post-tangle-hook 'recompile nil 'local)))))
-
- (nil . ((eval . (add-hook 'after-save-hook 'recompile nil 'local))
-         (compile-command . "nixos-rebuild switch")))
-)
+((nil . ((eval . (add-hook 'after-save-hook 'recompile nil 'local))
+         (compile-command . "nixos-rebuild switch"))))
