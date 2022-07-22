cc31cefea804c33973991ec154c4133e1db513fa
Michael Pontus <m.pontus@gmail.com>
2022-07-22 22:19:47 +0300

    changes

--- vm.nix
+++ vm.nix
@@ -45,8 +45,12 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.windowManager.xmonad = {
+  #   enable = true;
+  #   enableContribAndExtras = true;
+  # };
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
