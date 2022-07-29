c5538e1297284a61aa2391fc0c46deca2c3988e1
Michael Pontus <m.pontus@gmail.com>
2022-07-29 14:03:19 +0300

    Delete .dir-locals to stop recompile on save

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((eval . (add-hook 'after-save-hook 'recompile nil 'local))
-         (compile-command . "nixos-rebuild switch"))))
